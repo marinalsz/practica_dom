9894457a0bf524cc0b52d988a64a121361342418
Marina <marina.lsz522@gmail.com>
2024-08-28 13:17:04 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const products = [
+  {
+    name: "HP Essentials 255 G8 AMD",
+    price: 289,
+    stars: 4,
+    reviews: 250,
+    seller: "PcComponentes",
+    image:
+      "https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg",
+  },
+  // Añade aquí al menos 9 productos más para tener un total de 10 productos
+  // puedes cambiar los campos de cada objeto si es necesario para tu diseño...
+];
